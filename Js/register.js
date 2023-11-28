9472026151ebf83eeeec35426c75b7a953bd32bc
Marcestordeur <marcstord@gmail.com>
2023-11-28 20:07:55 -0300

    fix login-register

--- Js/register.js
+++ Js/register.js
@@ -1,29 +1,33 @@
 const createUser = (event) => {
   event.preventDefault();
-  const name = document.getElementById('nombre').value
-  const email = document.getElementById('Email').value
-  const password = document.getElementById('Password').value
+  const name = document.getElementById('nombre').value;
+  const email = document.getElementById('Email').value;
+  const password = document.getElementById('Password').value;
+  const passwordconfirm = document.getElementById('contraseña').value;
 
   if (name.trim() === '' || email.trim() === '' || password.trim() === '') {
     alert("Por favor complete todos los campos");
     return;
   }
-
-  fetch('http://localhost:3000/users',{
-    method: 'POST',
-    body: JSON.stringify({
-      name,
-      email,
-      password,
-      role:'client'
-    }),
-    headers: {
-      'content-type': 'application/json; charset=UTF-8'
-    }
-  })
-  const myModal = new bootstrap.Modal(document.getElementById('ModalFuncional'),{});
-  myModal.show()
-  
+  if(password === passwordconfirm){
+    fetch('http://localhost:3000/users',{
+      method: 'POST',
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+        role:'client'
+      }),
+      headers: {
+        'content-type': 'application/json; charset=UTF-8'
+      }
+    })
+    const myModal = new bootstrap.Modal(document.getElementById('ModalFuncional'),{});
+    myModal.show() 
+  }else{
+    const myModal = new bootstrap.Modal(document.getElementById('ModalError'),{});
+    myModal.show() 
+  }
 } ;
 document.getElementById('registerForm').addEventListener('submit', createUser)
 
